c69d2b42ec0ceeb17a243d574500b84eacbc1664
mayblesssoi <81284019+mayblesssoi@users.noreply.github.com>
2022-04-24 15:09:46 +0900

    방송통신대학교 파이썬 실습 예제 1일차
    1. math 예제 (print했을때 문자열로 출력할 것인지 계산식으로 처리할 것인지)
    2. turtle 예제 (turtle 프로그램으로 그림그리기)
       -turtle.showturtle(현재 화살표 위치 표시 시작)

---
+++ example_turtle_basic1.py
@@ -0,0 +1,19 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:/Users/seoul/Desktop/knou python/math.py =============
+3.5 * 4 / 2 - 2.5 =
+4.5
+d
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    d
+NameError: name 'd' is not defined. Did you mean: 'id'?
+import turtle
+turtle.showturtle()
+turtle.goto(0, 50)
+turtle.penup()
+turtle.goto(50,-50)
+turtle.pendown()
+turtle.color("red")
+turtle.circle(50)
